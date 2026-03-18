3466e6d637d33b3e53c50b5963016d98961d6d97
gucci1909 <umangarora0134@gmail.com>
2026-03-18 10:07:05 +0530

    added files for generation for reels

--- generation_files.py
+++ generation_files.py
@@ -1,13 +1,20 @@
-
+import os;
 
 def text_to_audio(folder):
-    pass;
+    print(folder);
     
-def create_reel():
-    pass;
+def create_reel(folder):
+    print(folder);
 
 if __name__ == "__main__":
-    folder = "588...";
-    text_to_audio(folder);
-    create_reel(folder);
-    pass;
+    with open("done.txt", "r") as f:
+        done_folders = f.readlines();
+        
+    done_folders = [f.strip() for f in done_folders]; 
+    folders = os.listdir("user_uploads");
+    for folder in folders:
+        if(folder not in done_folders):
+            text_to_audio(folder);
+            create_reel(folder);
+            with open("done.txt", "a") as f:
+                f.write(folder + "\n");
