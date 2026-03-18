61205b3213a66e86ac36efd0544efacdb129e9ea
gucci1909 <umangarora0134@gmail.com>
2026-03-18 09:04:52 +0530

    added files for generation for reels

---
+++ generation_files.py
@@ -0,0 +1,13 @@
+
+
+def text_to_audio(folder):
+    pass;
+    
+def create_reel():
+    pass;
+
+if __name__ == "__main__":
+    folder = "588...";
+    text_to_audio(folder);
+    create_reel(folder);
+    pass;
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request
-import uuid;
-import os;
+import uuid
+import os
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'user_uploads';
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'};
+UPLOAD_FOLDER = 'user_uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/") 
+@app.route("/")
 def home():
     return render_template("index.html")
 
@@ -24,20 +24,21 @@ def create():
         for key, value in request.files.items():
             print(key, value)
             # upload files here
-            file = request.files[key];
+            file = request.files[key]
             if file:
-                filename = secure_filename(file.filename);
+                filename = secure_filename(file.filename)
                 os.makedirs(folder_path, exist_ok=True)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], recorded_id, filename));
-            
-            with open(os.path.join(app.config["UPLOAD_FOLDER"], recorded_id, "description.txt"),"w") as f:
-                f.write(description);
-                
+                file.save(os.path.join(
+                    app.config['UPLOAD_FOLDER'], recorded_id, filename))
+
+            with open(os.path.join(app.config["UPLOAD_FOLDER"], recorded_id, "description.txt"), "w") as f:
+                f.write(description)
 
     return render_template("create.html", myid=myid)
 
 @app.route("/gallery")
 def gallery():
-    return render_template("gallery.html")
+    return render_template("gallery.html");
+
 
 app.run(debug=True)
